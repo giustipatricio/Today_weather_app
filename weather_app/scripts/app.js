faac5b34fbe06188116075d4037661fe0b7be2ce
Patricio Giusti <giusti.patricio@gmail.com>
2020-01-31 17:47:28 -0300

    mod Class

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -3,7 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
-
+const forecast = new Forecast();
 
 const updateUI = (data) => {
   // const cityDets = data.cityDets;
@@ -25,13 +25,16 @@ const updateUI = (data) => {
 const iconSrc = `img/icons/${weather.WeatherIcon}.svg` ;
 icon.setAttribute('src', iconSrc);
 
+// let timeSrc = null;
+// if(weather.IsDayTime){
+  //   timeSrc = 'img/day.svg';
+  // } else {
+    //   timeSrc = 'img/night.svg';
+    // } Modo largo
 
-let timeSrc = null;
-if(weather.IsDayTime){
-  timeSrc = 'img/day.svg';
-} else {
-  timeSrc = 'img/night.svg';
-}
+const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg' ;//modocorto
+
+
 time.setAttribute('src', timeSrc);
 
 
@@ -47,28 +50,25 @@ if(card.classList.contains('d-none')){
 }
 
 
-const updateCity = async (city) => {
-
-  const cityDets = await getCity(city);
-  const weather = await getWheather(cityDets.Key);
-
-return {
-  cityDets: cityDets, // se puede borrar uno de los dos y sigue funcionando, cuando propiedad y valor son iguales.
-  weather: weather
-
-  };
-
-};
-
-
 cityForm.addEventListener('submit', e => {
   e.preventDefault();
+
   //obtener el valor de la cudad
+
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  updateCity(city)
+  forecast.updateCity(city)
   .then(data => updateUI(data))
   .catch(err => console.log(err));
 
+//set local storage
+localStorage.setItem('city', city);
+
 });
+
+if (localStorage.getItem('city')) {
+    forecast.updateCity(localStorage.getItem('city'))
+      .then(data => updateUI(data))
+      .catch(err => console.log(err));
+};
--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,29 +1,25 @@
-const key = 'VwnFdWlmzAZ9jpFYOngggd2Njt1RAns0';
+class Forecast {
+  constructor() {
+    this.key = 'VwnFdWlmzAZ9jpFYOngggd2Njt1RAns0';
+    this.weatherURL = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    this.cityURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+  }
+  async updateCity(city) {
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWheather(cityDets.Key);
+    return { cityDets, weather };  // cityDets: cityDets, // se puede borrar uno de los dos y sigue funcionando, cuando propiedad y valor son iguales. // weather: weather
+  }
+  async getCity(city){
+    const query = `?apikey=${this.key}&q=${city}` // ? agrega info como query
+    const response = await fetch(this.cityURL + query)
+    const data = await response.json();
+    return (data[0]); //trae el match mas cercano a lo escrito
+  }
+  async getWheather(id) {
+    const query = `${id}?apikey=${this.key}` // ? agrega info como query
+    const response = await fetch(this.weatherURL + query)
+    const data = await response.json(); //promesa
+    return (data[0]); //trae el match mas cercano a lo escrito
+  }
 
-
-const getWheather = async (id) => {
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-  const query = `${id}?apikey=${key}` // ? agrega info como query
-  const response = await fetch(base + query)
-  const data = await response.json(); //promesa
-  return (data[0]); //trae el match mas cercano a lo escrito
-
-};
-
-//getcity information
-const getCity = async (city) => {
-
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  const query = `?apikey=${key}&q=${city}` // ? agrega info como query
-  const response = await fetch(base + query)
-  const data = await response.json();
-  return (data[0]); //trae el match mas cercano a lo escrito
-};
-
-
-// getCity('manchester').then(data => {
-//    return getWheather(data.Key)
-//  }).then(data => {
-//    console.log(data)
-//  })
-//  .catch(err => console.log(err));
+}
